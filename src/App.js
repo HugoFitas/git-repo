626b1188feb0ad7ea5aee71ef47dd1a670337b8d
Hugo Fitas <hfitas@gmail.com>
2021-04-20 14:02:09 +0100

    added button to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button>Click Me!</button>
+    </div>
+  );
 }
 
 export default App;
