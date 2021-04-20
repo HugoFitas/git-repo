05310f6ab1bed5358708a287fbf01e2f927e1c87
Hugo Fitas <hfitas@gmail.com>
2021-04-20 14:04:18 +0100

    turned button into a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <button>Click Me!</button>
+      <Button />
     </div>
   );
 }
