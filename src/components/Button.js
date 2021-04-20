97037d61671935ec6b381470669834cf6397fee4
Hugo Fitas <hfitas@gmail.com>
2021-04-20 14:30:55 +0100

    created clickHandler function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Button = () => {
+  const clickHandler = () => {
+    alert("Hello World!");
+  };
   return (
     <div className="button-div">
       <button>Click Me!</button>
